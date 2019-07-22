107be3c85b731fd74ad9dcea1f7b2b3e96e366c7
Tzvi <52079980+taviv@users.noreply.github.com>
2019-07-22 14:50:10 -0400

    ada model

--- get_sepsis_score.R
+++ get_sepsis_score.R
@@ -1,23 +1,40 @@
 #!/usr/bin/Rscript
 # Tzvi Aviv tzvika.aviv@gmail.com
-#this model was trained with imputed NAs
- 
-library(xgboost)
+#scoring with an 12-hr sliding window adaboost model trained on setA plus B July 21th
+# fixed mistakes in data pitching function 
+library(fastAdaboost)
 
 get_sepsis_score = function(data, model){
+num_row<-nrow(tail(data,12))
+
+if (num_row <= 6) {return(as.matrix(cbind(score=0,label=0)))}
+
+else {
    
-#m = nrow(data)
-#get these "ICULOS","Age", "HR", "MAP", "O2Sat", "SBP", "Resp", "Temp", "Potassium", "Hct")
-    x = as.matrix(data[, c(40, 35, 1, 5, 2, 4, 7, 3, 26, 29)])
-    
-    score = tail(predict(model,x),1)
-    label = score > 0.1 
+temp<- matrix(data=NA,nrow=12,ncol=40)    
+temp[1:num_row,]<-tail(data[,1:40, drop=T],num_row)
+
+mean<-apply(temp,2,mean,na.rm=T)
+sd<-apply(temp,2,sd,na.rm=T)
+sat_fi<-mean[2]/mean[11]
+sat_fi[is.infinite(sat_fi)] = 5000
+plat_wbc<-mean[34]/mean[32]
+
+x<-as.matrix(c(mean,sat_fi,plat_wbc,sd),1,82)
+x<-t(x)
+x<-data.frame(x)
+pred = predict(model,x[1,])
+score<-pred$prob[2]
+#label = score > 0.5
+label <- as.numeric(as.character(pred$class))
+
     predictions = as.matrix(cbind(score, label)) 
     return(predictions)
 }
+}
 
 load_sepsis_model = function(){
-xgb.load("xgboost7_ta.model")
-   # return(NULL)
+readRDS("ada12hrAB.model")
+
 }
 
