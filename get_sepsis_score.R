98567b0d8e40b718f0d314dde5368dcdbb2a9333
Tzvi <52079980+taviv@users.noreply.github.com>
2019-08-18 21:36:43 -0400

    best model aug18

--- get_sepsis_score.R
+++ get_sepsis_score.R
@@ -1,18 +1,19 @@
 #!/usr/bin/Rscript
 # Tzvi Aviv tzvika.aviv@gmail.com
-#scoring with an 12-hr sliding window model trained in xgboost on setA and B centered July 29th
-# center only without saving the entire dataset will be done on the new data 
-#try to speed the script with matrixStats
+#scoring with an 12-hr sliding window model trained in xgboost on setA and B 
+#aug 18 - best model
+
 library(xgboost)
 library(matrixStats)
 
 counter<-1
 col_sums<-matrix(0,1,82)
-#col_sum_dif<-matrix(0,1,82)
+col_sum_dif<-matrix(0,1,82)
 
 get_sepsis_score = function(data, model){
 counter <<- counter+1
-#print(counter)   
+#print(counter)
+gender<-max(data[,36])     
 temp<- matrix(data=NA,nrow=12,ncol=40)
 num_row<-nrow(utils::tail(data,12))    
 temp[1:num_row,]<-utils::tail(data[,1:40, drop=T],num_row)
@@ -29,22 +30,22 @@ xtag<-x
 xtag[is.na(xtag)] <- 0
 col_sums <<- col_sums + xtag
 colmeans<-col_sums/(counter-1)
-#coldiff<-(x-colmeans)^2
-#colvar<-coldiff
-#colvar[is.na(colvar)]<-0  
-#col_sum_dif<<-col_sum_dif + colvar
-#colsds<-sqrt(col_sum_dif/(counter-1))
+coldiff<-(x-colmeans)^2
+colvar<-coldiff
+colvar[is.na(colvar)]<-0  
+col_sum_dif<<-col_sum_dif + colvar
+colsds<-sqrt(col_sum_dif/(counter-1))
 
-x<-data.frame(x-colmeans)#x-colmeans)/colsds)
+x<-data.frame((x-colmeans)/colsds)
 
 
 #select top 20 variables
 x<-x[,c('X40', 'X35', 'X39', 'X3', 'X1', 'X7', 'X4', 'X41' ,'X5', 'X2' ,'X43' ,'X6', 'X22', 'X44', 'X49', 'X46', 'X45', 'X47' ,'X32', 'X48')]
-
+x<-cbind(x,gender)
 
 
 score = predict(model,as.matrix(x))
-label = score > 0.03 #0.1
+label = score > 0.8
 
     predictions = as.matrix(cbind(score, label)) 
     return(predictions)
@@ -52,6 +53,6 @@ label = score > 0.03 #0.1
 }
 
 load_sepsis_model = function(){
-#xgb.load("xgb_windowAB_aug15")
-xgb.load("xgb_windowAB_center_aug16") #this is a lower variance model
+
+xgb.load("xgb_windowAB_aug18") 
 }
